f1b613617cafc5ce0d26e967315dd7607a720f17
BT22105087 <118710396+BT22105087@users.noreply.github.com>
2022-12-22 12:59:07 +0530

    Add files via upload

---
+++ 1 question.py
@@ -0,0 +1,15 @@
+#question 1
+st="Python is a case sensitive language"
+print("the length of string",len(st))   #a part
+for i in range (len(st)-1,-1,-1):       #b part
+    print(st[i], end="")
+s=st[10:26]         #c part
+print(s)
+print(st.replace(s,"object oriented"))      #d part
+print(st.index("a"))     #e part
+print(st.replace(" ",""))   #f part
+
+
+
+    
+    
